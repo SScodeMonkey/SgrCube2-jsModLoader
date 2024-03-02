7da03af42674f41964e09d6c3fe0f6f1d1366c75
SScodeMonkey <37500629+SScodeMonkey@users.noreply.github.com>
2024-03-02 15:59:13 +0700

    Delete mod directory
    replaced by mods

--- mod/js/config.js
+++
@@ -1,6 +0,0 @@
-var config = [ //disable module by commenting out its name in the array (comments in js are //)
-    "hotkeys",
-    "passageModder",
-],
-    testEnvironment = false,
-    setupInit = true;
--- mod/js/misc/position.js
+++
@@ -1,7 +0,0 @@
-function getOffset(element) { //not including padding and border
-	const viewport = $(element)[0].getBoundingClientRect();
-	return {
-		left: viewport.left + window.scrollX,
-		top: viewport.top + window.scrollY
-	};
-}
